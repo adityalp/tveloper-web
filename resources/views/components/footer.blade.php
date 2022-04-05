d4d675a6395326b034059a4378cabf0e1d199fc4
adityalp <adityalishadi03@gmail.com>
2022-04-05 21:02:43 +0700

    initial commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,44 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer">
+
+     <div class="footer-newsletter">
+         <div class="footer-top">
+             <div class="container">
+                 <div class="row">
+
+                     <div class="col-lg-6 col-md-6 footer-contact">
+                         <h3>
+                             <img src="{{ asset('assets/img/logo/logo_190.png') }}" alt="logo" height="100px" />
+                         </h3>
+                         <p>
+                             Kopo 330, Bandung<br>
+                             Indonesia <br><br>
+                             <strong>Phone:</strong> +62 895 606 173 288<br>
+                             <strong>Email:</strong> info@tveloper.id<br>
+                         </p>
+                     </div>
+
+                     <div class="col-lg-6 col-md-6 footer-links">
+                         <h4>Our Social Networks</h4>
+                         <p>Keep connected with us outside this company profile site</p>
+                         <div class="social-links mt-3">
+                             <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                             <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                             <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                             <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                             <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                         </div>
+                     </div>
+
+                 </div>
+             </div>
+         </div>
+
+         <div class="container footer-bottom clearfix">
+             <div class="copyright">
+                 &copy; Copyright <strong><span>tveloper</span></strong>. All Rights Reserved
+             </div>
+             <div class="credits">
+             </div>
+         </div>
+ </footer><!-- End Footer -->
