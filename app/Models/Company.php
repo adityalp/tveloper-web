170826561c45f338e05b0a5e04cedae3157d6c7b
Abdul Salam <abdulsalam121196@gmail.com>
2022-10-01 20:03:32 +0700

    ADD: base of model

---
+++ app/Models/Company.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $table = 'companies';
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+}
