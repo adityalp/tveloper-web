f4e41ccb68cca15c9915fa7756d1598cb46add08
Abdul Salam <abdulsalam121196@gmail.com>
2022-10-02 09:18:27 +0700

    REF: adjust model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,7 +13,7 @@ class Category extends Model
     protected $table = 'categories';
     protected $primaryKey = 'id';
     protected $keyType = 'string';
-
+    protected $fillable = ['title', 'slug', 'description', 'is_active'];
     /**
      * Get all of the portfolios for the Category
      *
